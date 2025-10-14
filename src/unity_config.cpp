f266e8457fed06b40fe274197a8c4827f24467a8
Mark Stanley <1367957+Nullkraft@users.noreply.github.com>
2025-10-14 11:10:17 -0400

    Add comments describing test vs pio run
    The use of negative logic to include/exclude unity_config.cpp was
    confusing. When the file is included, which only happens when tests
    are not being run, the line that includes unity_config.h makes it
    seem as though tests are being re-included.
    
    The extra comments clarify that unity_config.h is only there to
    implement any per-project macros that you may have defined. If you
    don't have any then that line is misleading/redundant.
    
    You can just ignore it.

--- src/unity_config.cpp
+++ src/unity_config.cpp
@@ -1,5 +1,29 @@
+/* During 'pio test', PlatformIO's test runner provides unity_config.cpp
+ *
+ * During 'pio run', you provide unit_config.cpp but it must be hidden
+ * when you are running 'pio test' so theirs can take over.
+ */
+
+/* *** If PIO_UNIT_TESTING IS defined ***
+ * Use serial configuration provided by platformio.ini...
+ *
+ * 'pio test' build
+ *   '--> PlatformIO defines PIO_UNIT_TESTING
+ *        '--> Your unity_config.cpp is excluded
+ *             '--> PlatformIO's implementation handles Unity output
+ *                  '--> Your unit_config.h still provides per-project macros
+*/
 #ifndef PIO_UNIT_TESTING
 
+/* *** If PIO_UNIT_TESTING is NOT defined ***
+ * Compile this unity_config.cpp so you can use actual hardware serial...
+ *
+ * 'pio run -e uno' or 'pio run -e uno_example' builds
+ *   '--> PIO_UNIT_TESTING is not defined
+ *        '--> Your unity_config.cpp is compiled
+ *             '--> Your implementation handles Unity output
+ *                  '--> Your unit_config.h still provides per-project macros
+ */
 #include "unity_config.h"
 
 #ifdef ARDUINO
