5d1eae37ca48a356ca4b1343a8f4c4f0336358b8
Mark Stanley <1367957+Nullkraft@users.noreply.github.com>
2026-01-07 09:45:16 -0500

    Commit specann_wrapper.cpp
    This wrapper provides the ability to use PlatformIO to compile Arduino
    sketch files with extensions .ino

---
+++ src/specann_wrapper.cpp
@@ -0,0 +1,5 @@
+#ifdef SPECANN_BUILD
+// Wrap the Arduino example sketch so PlatformIO can compile it as part of the
+// standard project sources.
+#include "../examples/specAnn/specAnn.ino"
+#endif
