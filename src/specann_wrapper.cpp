3d0a6bd6e192f68d21110ad7a45c7927a2866dcf
Mark Stanley <1367957+Nullkraft@users.noreply.github.com>
2026-01-07 16:07:47 -0500

    Fix GitHub Arduino CLI build error

--- src/specann_wrapper.cpp
+++ src/specann_wrapper.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 
-#if !defined(UNIT_TEST) && !defined(PIO_UNIT_TESTING) && !defined(CI_SMOKE_BUILD)
+#if defined(SPECANN_BUILD) && !defined(UNIT_TEST) && !defined(PIO_UNIT_TESTING) && !defined(CI_SMOKE_BUILD)
 // Wrap the Arduino example sketch so PlatformIO can compile it as part of the
 // standard project sources.
 #include "../examples/specAnn/specAnn.ino"
